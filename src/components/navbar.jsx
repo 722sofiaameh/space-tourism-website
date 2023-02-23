1e3c837490318e4b9ad9f753d24df8c5398bc9ed
722sofiaameh <sofiagabriel722@gmail.com>
2023-02-23 08:05:32 -0800

    ..

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import Logo from '../assets/shared/logo.svg'
+import Hamburger from '../assets/shared/icon-hamburger.svg'
+import close from '../assets/shared/icon-close.svg'
 
 
 const Navbar = () =>
@@ -12,8 +15,8 @@ const Navbar = () =>
    ]
    return (
       <nav className="flex items-center justify-between pt-5">
-         <img src={open ? "./src/assets/shared/icon-close.svg" : "./src/assets/shared/icon-hamburger.svg"} className="md:hidden  fixed right-5 cursor-pointer z-20 top-6" onClick={() => setopen(!open)} />
-         <img src="./src/assets/shared/logo.svg" alt="logo" className="w-10 ml-7" />
+         <img src={open ? close : Hamburger} className="md:hidden  fixed right-5 cursor-pointer z-20 top-6" onClick={() => setopen(!open)} />
+         <img src={Logo} alt="logo" className="w-10 ml-7" />
          <ul className={`bg-[#ffffff14] backdrop-blur-md  md:pl-10 pr-28 md:static fixed duration-500 ease-linear top-0 md:h-auto h-screen z-10 ${!open ? 'right-[-100%] ' : 'right-0'}`}>
             {
                menus.map((menu, index) => (
