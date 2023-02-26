ac9df0d9aaa8a4ae6b78bdc390db7e8207140121
722sofiaameh <sofiagabriel722@gmail.com>
2023-02-26 10:24:42 -0800

    chore : code clean up

--- src/crew.jsx
+++ src/crew.jsx
@@ -10,7 +10,7 @@ const Crew = () => {
   return (
     <div className="bg-[url('../public/assets/crew/background-crew-mobile.jpg')] h-screen bg-cover bg-no-repeat md:bg-[url('../src/assets/crew/background-crew-desktop.jpg')] md:h-screen md:bg-cover md:bg-no-repeat p-4">
       <Navbar />
-      <div className="flex text-white gap-4 items-center justify-center mt-4 font-Barlow uppercase text-xl">
+      <div className="flex text-white gap-4 items-center justify-center font-Barlow uppercase text-xl">
         <h1 className="opacity-[0.25]">02</h1>
         <h1 className="">Meet your crew</h1>
       </div>
